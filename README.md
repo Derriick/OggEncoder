8fe1856fa0f81debae77e6eccee549c5f74501a0
Derriick <derriick.ensiie@yahoo.com>
2018-07-18 21:15:05 +0200

    Adding OPUS and MP3 support

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # OggEncoder
-A Python **3** script to encode OGG files with oggenc and keeping the file tree
+A Python **3** script to encode OGG, OPUS and MP3 files with keeping the file tree
 
 ## Usage
-The **vorbis-tools** package must be installed. This script does not require any additional module.
+For each encoder, its corresponding package must be installed:
+- **vorbis-toos** for VORBIS codec
+- **opus-tools** for OPUS codec
+- **lame** for MP3 codec
+This script does not require any additional module.
 
 ### Command
 For a simple use, the command can be launched without option
@@ -13,8 +17,13 @@ For a simple use, the command can be launched without option
 ### Options
 | Option                   | Description                                    |
 |--------------------------|------------------------------------------------|
-| `-s, --source`      | Specify the input directory                         |
-| `-d, --destination` | Specify the output directory                        |
+| `-i, --input`       | Specify the source directory                        |
+| `-o, --output`      | Specify the destination directory                   |
 | `-q, --quality`     | Specify quality, between -1 (very low) and 10 (very high).<br>Fractional qualities (e.g. 2.75) are permitted.<br>The default quality level is 3. |
+| `-b, --bitrate`     | Specify bitrate value to use instead of quality     |
 | `-e, --extension`   | Speficy the extension of the files to process (default `flac` and `wav`) |
+| `-s, --source`      | Speficy the extension of the files to process (default flac and wav) |
+| `-d, --destination` | Speficy the extension of the destination files (default corresponds to chosen encoder) |
 | `-c, --copy`        | Copy the other files from `source` to `destination` |
+| `-f, --force`       | Overwrite existing files                            |
+  
--- oggenc.py
+++ oggenc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import subprocess
 from os import getcwd, makedirs, listdir
 from os.path import exists, isfile, isdir, splitext, abspath
@@ -11,58 +12,158 @@ DEFAULT_FILE_TYPES = ("flac", "wav")
 
 # Génère les options de la commande
 p = OptionParser()
-p.add_option("-s", "--source", dest="source", default=".",
-             help="Specify the input directory")
-p.add_option("-d", "--destination", dest="destination", default=".",
-             help="Specify the output directory")
-p.add_option("-q", "--quality", dest="quality", default="3",
+p.add_option("-i", "--input", dest="inp", default=".",
+             help="Specify the source directory")
+p.add_option("-o", "--output", dest="out", default=".",
+             help="Specify the destination directory")
+p.add_option("-q", "--quality", dest="quality",
              help="Specify quality, between -1 (very low) and 10 (very high).\nFractional qualities (e.g. 2.75) are permitted.\nThe default quality level is 3.")
-p.add_option("-e", "--extension", dest="extension", default="*",
-        help="Speficy the extension of the files to process (default flac and wav)")
+p.add_option("-b", "--bitrate", dest="bitrate",
+             help="Specify bitrate value to use instead of quality")
+p.add_option("-e", "--encoder", dest="encoder", default="ogg",
+             help="Specify the encoder to use for the conversion")
+p.add_option("-s", "--source", dest="source", default="*",
+             help="Speficy the extension of the files to process (default flac and wav)")
+p.add_option("-d", "--destination", dest="destination",
+             help="Speficy the extension of the destination files (default corresponds to chosen encoder)")
 p.add_option("-c", "--copy", action="store_true", dest="copy", default=False,
-             help="Copy the other files from source to destination")
+             help="Copy the other files from source to destination directory")
+p.add_option("-f", "--force", action="store_true", dest="force", default=False,
+             help="Overwrite existing files")
 options, args = p.parse_args()
 
-# Encode tous les fichiers de "destination" récursivement,
+def is_number(n):
+    if n == None:
+        return False
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
+def get_encoder(encoder):
+    if encoder == 1 :
+        return ["oggenc"]
+    elif encoder == 2 :
+        return ["opusenc"]
+    elif encoder == 3 :
+        return ["lame"]
+
+def get_quality(encoder) :
+    if options.quality == None and options.bitrate == None :
+        return []
+    elif encoder == 1 :
+        if is_number(options.bitrate) :
+            return ["--bitrate", options.bitrate]
+        elif options.bitrate == None and is_number(options.quality) :
+            return ["--quality", options.quality]
+    elif encoder == 2 :
+        if options.quality != None or options.bitrate == None :
+            print ("Quality cannot be specified with opusenc")
+            sys.exit(2)
+        elif is_number(options.bitrate) :
+            return ["--bitrate", options.bitrate]
+    elif encoder == 3 :
+        if options.quality == None :
+            if options.bitrate == None :
+                print ("Specify quality or bitrate")
+                sys.exit(2)
+            elif is_number(options.bitrate) :
+                return ["-B", options.bitrate]
+        elif options.bitrate == None :
+            if options.quality == "h" :
+                return ["-h"]
+            elif options.quality == "f" :
+                return ["-f"]
+            elif options.quality == "v" :
+                return ["-v"]
+            elif options.quality == "medium" :
+                return ["--preset", "medium"]
+            elif options.quality == "standard" :
+                return ["--preset", "standard"]
+            elif options.quality == "extreme" :
+                return ["--preset", "extreme"]
+            elif options.quality == "insane" :
+                return ["--preset", "insane"]
+            elif is_number(options.quality) :
+                return ["-V", options.quality]
+    print ("Wrong quality or bitrate")
+    sys.exit(2)
+
+def get_extension(encoder):
+    if options.destination == None :
+        if encoder == 1 :
+            return ".ogg"
+        elif encoder == 2 :
+            return ".opus"
+        elif encoder == 3 :
+            return ".mp3"
+    else :
+        return '.' + options.destination
+
+# Encode tous les fichiers de "out" récursivement,
 # en gardant la même arborescence de fichiers
-def encode(source, destination):
-    files = [d for d in listdir(source) if not d.startswith(".")]
+def encode(inp, out, base_cmd, encoder, extension) :
+    files = [d for d in listdir(inp) if not d.startswith(".")]
 
     # Vérifie si le dossier existe déjà, sinon le crée
-    if not isdir(destination):
+    if not isdir(out) :
         # Vérifie qu'il n'existe pas un fichier du même nom
-        if exists(destination):
-            print ("File already exists: " + destination)
+        if exists(out) :
+            print ("File already exists: " + out)
             return  # On quitte a fonction
-        makedirs(destination)
+        makedirs(out)
 
     # Gère chaque fichier et dossier séparément
-    for f in files:
-        path = source + '/' + f
+    for input_file in files:
+        path = inp + '/' + input_file
 
-        # Vérfie le type de "f"
-        if isdir(path):
+        # Vérfie le type de input_file
+        if isdir(path) :
             # Appelle la fonction "encode" récursivement
-            encode(path, destination + '/' + f)
-        elif isfile(path):
-            name, ext = splitext(f)
+            encode(path, out + '/' + input_file, base_cmd, encoder, extension)
+        elif isfile(path) :
+            name, ext = splitext(input_file)
+            output_file = out + '/' + name + extension
 
-            # Vérifie l'extension du fichier à encoder
-            if ext[1:] in options.extension:
-                # Génère et lance la commande shell
-                cmd = ['oggenc', '-q' + options.quality , path, '-o', destination + '/' + name + '.ogg']
-                subprocess.check_call(cmd, cwd=getcwd())
-            elif options.copy:
-                # Copie simplement le fichier
-                cmd = ['cp', path, destination + '/' + f]
-                subprocess.check_call(cmd, cwd=getcwd())
-        else:
+            if not exists(output_file) or options.force :
+                # Vérifie l'extension du fichier à encoder
+                if ext[1:] in options.source:
+                    # Génère et lance la commande shell
+                    if encoder == 1 :
+                        cmd = base_cmd + [path, '-o', output_file]
+                    elif encoder == 2 :
+                        cmd = base_cmd + [path, output_file]
+                    elif encoder == 3 :
+                        cmd = base_cmd + [path, output_file]
+                    subprocess.check_call(cmd, cwd=getcwd())
+                elif options.copy:
+                    # Copie simplement le fichier
+                    cmd = ['cp', path, out + '/' + input_file]
+                    subprocess.check_call(cmd, cwd=getcwd())
+            else :
+                print (output_file + " already exists")
+        else :
             print ("Error: " + path)
 
-if options.extension == '*':
-    options.extension = DEFAULT_FILE_TYPES
+if "ogg" in options.encoder or "vorbis" in options.encoder :
+    encoder = 1
+elif "opus" in options.encoder :
+    encoder = 2
+elif "lame" in options.encoder or "mp3" in options.encoder :
+    encoder = 3
+else :
+    print (options.encoder + " is not a valid encoder")
+    sys.exit(1)
 
-source = abspath(options.source)
-destination = abspath(options.destination)
+base_cmd = get_encoder(encoder) + get_quality(encoder)
 
-encode(source, destination)
+if options.source == '*' :
+    options.source = DEFAULT_FILE_TYPES
+
+extension = get_extension(encoder)
+
+inp = abspath(options.inp)
+out = abspath(options.out)
+
+encode(inp, out, base_cmd, encoder, extension)
